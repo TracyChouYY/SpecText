4d2691b453f92f20ce0cb1be4b273344ab7fd599
TracyChouYY <zhouyuan_yyl@126.com>
2017-08-22 16:35:26 +0800

    TextTracy

---
+++ PodSepcText/PodSepcText/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PodSepcText
+//
+//  Created by minfo019 on 2017/8/22.
+//  Copyright © 2017年 Tracy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
